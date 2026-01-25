220ca400a750215c225bd86ee78a18fc4a66b2a9
Görkem Ege Zor <egezor4@gmail.com>
2026-01-25 05:32:54 +0300

    Initial Commit

---
+++ ArchiLens/ArchiLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiLensApp.swift
+//  ArchiLens
+//
+//  Created by Görkem Ege Zor on 25.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct ArchiLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
